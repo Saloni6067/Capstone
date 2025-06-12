2cda01b608e83fa884d2500ff673528d89f56bd3
Saloni6067 <salontripathi982@gmal.com>
2025-06-12 01:38:17 -0400

    yaml changes

--- src/train.py
+++ src/train.py
@@ -4,19 +4,19 @@
 import argparse
 import os
 import tempfile
-
+import tensorflow as tf
+from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 from google.cloud import storage
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping
 
 
 def download_from_gcs(bucket_name: str, prefix: str, local_dir: str):
